f714ccd8a6379c24a45e60418ed1e52b2a6dd6a5
hazalnurasma <asmahazalnur@gmail.com.tr>
2024-03-13 20:31:44 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './Components/SpotifyEmbedded';
+import './App.css';
+import SpotifyEmbedded from './Components/SpotifyEmbedded';
+
+function App() {
+  return (
+    <SpotifyEmbedded />
+  );
+}
+
+export default App;
---
+++ src/Components/SpotifyEmbedded.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const SpotifyEmbedded = () => {
+  return (
+    <div>
+      <iframe
+        title="Spotify Embed"
+        src="https://open.spotify.com/embed/album/3BIiOsLIJBsH9UJYFRqfxs?utm_source=generator"
+        width="50%"
+        height="370"
+        frameBorder="0"
+        allowFullScreen
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+        style={{ borderRadius: '12px' }}
+      ></iframe>
+    </div>
+  );
+};
+
+export default SpotifyEmbedded;
