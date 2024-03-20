2df988b8d1fcb01dcb1a7223ed6664f164d2b1a4
hazalnurasma <asmahazalnur@gmail.com.tr>
2024-03-20 18:58:05 +0300

    sticky and blury navbar is ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './Components/SpotifyEmbedded';
 import './App.css';
 import SpotifyEmbedded from './Components/SpotifyEmbedded';
-import { Content } from './Components/Content';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Content />
-      <SpotifyEmbedded />
-    </>
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Navbar />} /> */}
+        <Route path="/" element={<SpotifyEmbedded />} />
+      </Routes>
+
+    </Router>
 
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
+import './Navbar.css';
+import logo from '../Assets/logo.png';
 
 const Navbar = () => {
   return (
-    <>
+    <> 
+      <nav>
+
+        <div>
+          <img className="logo" src={logo} />
+        </div>
+
         <ul>
-            <li>
-                Music
-            </li>
-            <li>
-                Blog
-            </li>
+
+          <li>
+            <p>Home</p>
+          </li>
+          <li>
+            <p>Blog</p>
+          </li>
+          <li>
+            <p>Your Page</p>
+          </li>
         </ul>
+      </nav>
     </>
   )
 }
